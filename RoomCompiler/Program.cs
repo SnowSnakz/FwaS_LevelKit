aefeb91e8bfd5de6296fdb5ae3d7ed3964184f26
Woof Hybrid <snowsnakz@gmail.com>
2024-03-13 14:23:42 -0500

    Upload Project

---
+++ RoomCompiler/Program.cs
@@ -0,0 +1,142 @@
+﻿
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Diagnostics;
+using System.Text.Json;
+
+byte[] signature = [
+    0x4C,
+    0x59,
+    0x54,
+    0x00
+];
+
+if (args.Length <= 0)
+{
+    Console.WriteLine("Usage: RoomCompiler <file1> [<file2> <file3>...]");
+}
+
+Rgba32[] palette =
+[
+    new Rgba32(255,   0,   0, 255),
+    new Rgba32(193, 132,  84, 255),
+    new Rgba32( 44, 106,  31, 255),
+    new Rgba32(255, 255,   0, 255),
+    new Rgba32( 67,   0, 255, 255),
+    new Rgba32(115, 255, 249, 255),
+    new Rgba32( 90, 189,  99, 255),
+    new Rgba32(224, 189,  62, 255),
+    new Rgba32(140, 180,  78, 255)
+];
+
+foreach(var file in args)
+{
+    try
+    {
+        string path = Path.GetFullPath(file);
+        int lastSlash = path.LastIndexOf(Path.PathSeparator);
+
+        string outputFile;
+        if (lastSlash == -1) outputFile = "";
+        else outputFile = path[..lastSlash];
+
+        outputFile += Path.GetFileNameWithoutExtension(path) + ".dat";
+
+        using Image<Rgba32> image = Image.Load<Rgba32>(path);
+
+        Rgba32[] pixels = new Rgba32[484];
+        image.CopyPixelDataTo(pixels);
+        
+        if((image.Width != 22) || (image.Height != 22))
+            throw new NotSupportedException("Image Width/Height must be 22px/22px.");
+
+        using var output = File.OpenWrite(outputFile);
+        output.Write(signature);
+
+        bool unknownColor = false;
+
+        Rgba32 color = new Rgba32(0, 0, 0, 128);
+        int count = 0;
+
+        for(int i = 0; i < 484; i++)
+        {
+            Rgba32 pixel = pixels[i];
+
+            if (color != pixel)
+            {
+                if (count <= 0)
+                {
+                    color = pixel;
+                    count = 1;
+                    continue;
+                }
+
+                int val = Array.IndexOf(palette, color);
+                if (val == -1)
+                {
+                    val = 0;
+
+                    if (color.A != 0)
+                    {
+                        if (!unknownColor)
+                        {
+                            unknownColor = true;
+                            Console.WriteLine($"[W] {file} -> This image has an invalid pixel.");
+                        }
+                    }
+                }
+                else
+                {
+                    val++;
+                }
+
+                while (count > 0)
+                {
+                    byte dat = (byte)(val | ((Math.Min(count, 16) - 1) << 4));
+                    output.WriteByte(dat);
+                    count -= 16;
+                }
+
+                color = pixel;
+                count = 0;
+            }
+
+            count++;
+        }
+
+        if(count > 0)
+        {
+            int val = Array.IndexOf(palette, color);
+            if (val == -1)
+            {
+                val = 0;
+
+                if (color.A != 0)
+                {
+                    if (!unknownColor)
+                    {
+                        unknownColor = true;
+                        Console.WriteLine($"[W] {file} -> This image has an invalid pixel.");
+                    }
+                }
+            }
+            else
+            {
+                val++;
+            }
+
+            while (count > 0)
+            {
+                byte dat = (byte)(val | ((Math.Min(count, 16) - 1) << 4));
+                output.WriteByte(dat);
+                count -= 16;
+            }
+        }
+
+        Console.WriteLine($"[O] {file} -> {outputFile}");
+    }
+    catch(Exception exception)
+    {
+        Console.WriteLine($"[E] {file} -> {exception.GetType().FullName}: {exception.Message}");
+    }
+}
---
+++ RoomLinker/Program.cs
@@ -0,0 +1,80 @@
+﻿using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Usages: RoomLinker <OutputFile> <RoomFile1> [<RoomFile2> <RoomFile3>...]");
+    return;
+}
+
+byte[] signature = [
+    0x4C,
+    0x49,
+    0x42,
+    0x00
+];
+
+byte[] roomSignature = [
+    0x4C,
+    0x59,
+    0x54,
+    0x00
+];
+
+byte[] buf = new byte[4];
+
+Console.WriteLine($"╔╡ Writing to {args[0]}");
+
+using var output = new MemoryStream();
+output.WriteByte((byte)(args.Length - 1));
+
+int total = args.Length - 1;
+for(var i = 1; i < args.Length; i++)
+{
+    using var input = File.OpenRead(args[i]);
+    input.Read(buf);
+
+    if(buf.SequenceEqual(roomSignature))
+    {
+        input.CopyTo(output);
+
+        Console.WriteLine($"╟───┤ Appended {args[i]}.");
+    }
+    else if(buf.SequenceEqual(signature))
+    {
+        using var decompress = new InflaterInputStream(input);
+
+        using var lib = new MemoryStream();
+        decompress.CopyTo(lib);
+
+        lib.Read(buf);
+
+        int count = BitConverter.ToInt32(buf, 0);
+        total = total - 1 + count;
+
+        long pos = output.Position;
+        output.Position = 0;
+        output.WriteByte((byte)total);
+        output.Position = pos;
+
+        lib.CopyTo(output);
+
+        Console.WriteLine($"╟───┤ Appended {count} from {args[i]}.");
+    }
+    else
+    {
+        Console.WriteLine($"╟─x─┤ {args[i]} unsupported.");
+    }
+}
+
+Console.Write("╚═╡ Compressing... ");
+
+output.Position = 0;
+
+using var outFile = File.OpenWrite(args[0]);
+using var compressed = new DeflaterOutputStream(outFile);
+
+outFile.Write(signature);
+output.CopyTo(compressed);
+compressed.Flush();
+
+Console.WriteLine($"DONE! <{args[0]}>");
