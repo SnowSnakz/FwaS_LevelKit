8c31f2fa14ba57555f728793f8a257ea79c48490
Woof Hybrid <snowsnakz@gmail.com>
2024-03-13 15:12:08 -0500

    Expanded palette & Explained Colors.

--- RoomCompiler/Program.cs
+++ RoomCompiler/Program.cs
@@ -1,8 +1,5 @@
-﻿
-using SixLabors.ImageSharp;
+﻿using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
-using System.Diagnostics;
-using System.Text.Json;
 
 byte[] signature = [
     0x4C,
@@ -16,18 +13,25 @@ if (args.Length <= 0)
     Console.WriteLine("Usage: RoomCompiler <file1> [<file2> <file3>...]");
 }
 
-Rgba32[] palette =
-[
-    new Rgba32(255,   0,   0, 255),
-    new Rgba32(193, 132,  84, 255),
-    new Rgba32( 44, 106,  31, 255),
-    new Rgba32(255, 255,   0, 255),
-    new Rgba32( 67,   0, 255, 255),
-    new Rgba32(115, 255, 249, 255),
-    new Rgba32( 90, 189,  99, 255),
-    new Rgba32(224, 189,  62, 255),
-    new Rgba32(140, 180,  78, 255)
-];
+var palette = new Rgba32[16]
+{
+    new(  0,   0,   0, 255), // 0) Nothing
+    new(255,   0,   0, 255), // 1) Enemy Spawn  (The type of enemy that spawns depends on adjacent tiles. Depending on the current difficulty, some of these may be ignored.)
+    new(193, 132,  84, 255), // 2) Wall
+    new( 44, 106,  31, 255), // 3) Decoration   (Attempts to fill area with appropriate decoration tiles, some tiles may be replaced with Nothing.)
+    new(255, 255,   0, 255), // 4) Collectable  (Spawns as a Yellow Coin, unless level is a Shop.)
+    new( 67,   0, 255, 255), // 5) Tide Emitter (Only one is chosen at random.)
+    new(115, 255, 249, 255), // 6) Air Bubble   (Turns into an Air Bubble Emitter if adjacent to wall, becomes an Air Pocket if a 2x2 or larger area is defined.)
+    new( 90, 189,  99, 255), // 7) Platform     (Turns into a floating platform object if by itself.)
+    new(224, 189,  62, 255), // 8) Wall         (Turns into Nothing if the cloest edge is open.)
+    new(140, 180,  78, 255), // 9) Platform     (Turns into Nothing if the closest edge is closed.)
+    new( 84, 143,  39, 255), // A) Decoration   (Turns into Wall if the closest edge is closed.)
+    new( 79, 157,  93, 255), // B) Decoration   (Turns into Nothing if the closest edge is open.)
+    new(157,  59,  59, 255), // C) Spike Hazard
+    new(172,  99, 201, 255), // D) Purple Coin  (Spawns as a Yellow coin, turns purple after collecting.)
+    new(112,  75,  39, 255), // E) Secret Area  (A collisionless wall that fades when the player overlaps to reveal a hidden area.)
+    new(210, 167,   0, 255)  // F) Secret Coin  (Same as Secret Area except a Yellow Coin also spawns here.)
+};
 
 foreach(var file in args)
 {
